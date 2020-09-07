84069cc2b1c57362267be06c7719ff6fa07e125a
Nikolajs Korolkovs <k@nick.tf>
2020-09-07 15:26:58 +0300

    kyu6: DuplicateEncoder refactoring

--- java/src/kyu6/DuplicateEncoder.java
+++ java/src/kyu6/DuplicateEncoder.java
@@ -1,18 +1,18 @@
 package kyu6;
 
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class DuplicateEncoder {
-    static String encode(String word){
-        var count = word.toLowerCase().chars()
+    static String encode(String word) {
+        String lwrCaseWord = word.toLowerCase();
+        var charCounts = lwrCaseWord.chars()
             .mapToObj(c -> (char) c)
-            .collect(Collectors.toMap(
-                c -> c,
-                c -> 1,
-                Integer::sum
-            ));
-        return word.toLowerCase().chars()
-            .mapToObj(c -> count.get((char) c) > 1 ? ")" : "(")
+            .collect(Collectors.groupingBy(
+                Function.identity(),
+                Collectors.counting()));
+        return lwrCaseWord.chars()
+            .mapToObj(c -> charCounts.get((char) c) > 1 ? ")" : "(")
             .collect(Collectors.joining());
     }
 }
