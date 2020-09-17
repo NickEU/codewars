cd2a240d11752a187e8f03968a7a100738fea009
Nikolajs Korolkovs <k@nick.tf>
2020-09-17 09:05:10 +0300

    moved c# solution into a separate folder

---
+++ c#/Solutions/BitCountingTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Codewars
+{
+    [TestFixture]
+    public class BitCountingTest
+    {
+        [Test]
+        public void CountBits()
+        {
+            Assert.AreEqual(0, BitCounting.CountBits(0));
+            Assert.AreEqual(1, BitCounting.CountBits(4));
+            Assert.AreEqual(3, BitCounting.CountBits(7));
+            Assert.AreEqual(2, BitCounting.CountBits(9));
+            Assert.AreEqual(2, BitCounting.CountBits(10));
+        }
+    }
+}
