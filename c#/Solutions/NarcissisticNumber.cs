6cd7f2ef90bddf2950aa920e90159ed3204e4dfe
Nikolajs Korolkovs <k@nick.tf>
2020-09-23 09:55:32 +0300

    kyu6: c#: +NarcissisticNum

---
+++ c#/Solutions/NarcissisticNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Codewars
+{
+    public class NarcissisticNumber
+    {
+        public static bool IsNarcissistic(int valToTest)
+        {
+            int numOfDigits = valToTest.ToString().Length;
+            int narcissVal = 0;
+            int remainingVal = valToTest;
+            while (remainingVal > 0)
+            {
+                int nextDigit = remainingVal % 10;
+                remainingVal /= 10;
+                narcissVal += (int) Math.Pow(nextDigit, numOfDigits);
+            }
+            return narcissVal == valToTest;
+        }
+    }
+}
---
+++ c#/Solutions/NarcissisticTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars
+{
+    [TestFixture]
+    public class NarcissisticTest
+    {
+        private const int TestCase1Input = 1;
+        private const int TestCase2Input = 371;
+        private const int TestCase3Input = 1111;
+        private const int TestCase4Input = 55;
+        private const string TestSuccessOutput = " is narcissistic";
+        private const string TestFailureOutput = " is not narcissistic";
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                // success:
+                yield return new TestCaseData(TestCase1Input)
+                    .Returns(true)
+                    .SetDescription($"{TestCase1Input}{TestSuccessOutput}");
+                yield return new TestCaseData(TestCase2Input)
+                    .Returns(true)
+                    .SetDescription($"{TestCase2Input}{TestSuccessOutput}");
+                // failures:
+                yield return new TestCaseData(TestCase3Input)
+                    .Returns(false)
+                    .SetDescription($"{TestCase3Input}{TestFailureOutput}");
+                yield return new TestCaseData(TestCase4Input)
+                    .Returns(false)
+                    .SetDescription($"{TestCase4Input}{TestFailureOutput}");
+
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public bool Test(int n) => NarcissisticNumber.IsNarcissistic(n);
+    }
+}
