9634d701dac52c25a829a4583e6159ff208fd1ca
Nikolajs Korolkovs <k@nick.tf>
2020-09-23 10:14:55 +0300

    kyu7 dir structure

---
+++ c#/Solutions/kyu7/HighestAndLowest.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Codewars.kyu7
+{
+    class HighestAndLowest
+    {
+        public static string HighAndLow(string str)
+        {
+            var numbers = str.Split(" ")
+                .Select(int.Parse).ToList();
+            return $"{numbers.Max()} {numbers.Min()}";
+        }
+    }
+}
---
+++ c#/Solutions/kyu7/HighestAndLowestTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.kyu7
+{
+    class HighestAndLowestTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("42 -9",
+                HighestAndLowest.HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+            Assert.AreEqual("3 3",
+                HighestAndLowest.HighAndLow("3 3 3"));
+            Assert.AreEqual("-7 -22",
+                HighestAndLowest.HighAndLow("-7 -9 -22"));
+        }
+    }
+}
