5d518dd650124b31562c60b8679998ad4e94902d
Nikolajs Korolkovs <k@nick.tf>
2020-09-23 09:56:03 +0300

    kyu6: c#: +VowelCount

---
+++ c#/Solutions/VowelCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codewars
+{
+    class VowelCount
+    {
+        public static int GetVowelCount(string str)
+        {
+            return str.Count(c => "aoeui".Contains((char) c));
+        }
+    }
+}
---
+++ c#/Solutions/VowelCountTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Codewars
+{
+    class VowelCountTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            var errorMsg = "Wrong answer!";
+            Assert.AreEqual(5, VowelCount.GetVowelCount("abracadabra"), errorMsg);
+            Assert.AreEqual(1, VowelCount.GetVowelCount("a"), errorMsg);
+            Assert.AreEqual(0, VowelCount.GetVowelCount("brc"), errorMsg);
+        }
+    }
+}
