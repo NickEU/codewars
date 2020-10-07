3be552902d0f62f4b8660584fc92f972bc287bca
Nikolajs Korolkovs <k@nick.tf>
2020-10-07 08:15:49 +0300

    kyu5: +MovingZeroesToTheEnd c#

---
+++ c#/Solutions/kyu5/MovingZeroesToTheEnd.cs
@@ -0,0 +1,23 @@
+﻿namespace Codewars.kyu5
+{
+    internal class MovingZeroesToTheEnd
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            var arrLen = arr.Length;
+            // an array of integers is initialized with 0s 
+            var result = new int[arrLen];
+            var i = 0;
+
+            foreach (var num in arr)
+            {
+                if (num != 0)
+                {
+                    result[i++] = num;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ c#/Solutions/kyu5/MovingZeroesToTheEndTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.kyu5
+{
+    internal class MovingZeroesToTheEndTest
+    {
+        [Test]
+        public void Test1()
+        {
+            var expected = new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 };
+            var actual = MovingZeroesToTheEnd
+                .MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var expected = new int[] { 0, 0, 0, 0 };
+            var actual = MovingZeroesToTheEnd
+                .MoveZeroes(new int[] { 0, 0, 0, 0 });
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var expected = new int[] { 1, 2, 1, 1 };
+            var actual = MovingZeroesToTheEnd
+                .MoveZeroes(new int[] { 1, 2, 1, 1 });
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
